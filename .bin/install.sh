6fa5dbb29cfb76288990664db862e69c5c9e1f3e
Korede Aderele <kaderele@gmail.com>
2018-04-02 23:52:36 -0700

    Add install script

---
+++ .bin/install.sh
@@ -0,0 +1,16 @@
+# borrowed from: https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/ 
+git clone --bare https://github.com/korede-ta/dotfiles.git $HOME/.cfg
+function config {
+   /usr/local/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+	echo "Checked out config.";
+else
+	echo "Backing up pre-existing dot files.";
+	config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+
+config checkout
+config config status.showUntrackedFiles noinformation
